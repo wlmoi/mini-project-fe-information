f5695527bc33c6323a733f44cc4f8c910157a00d
William Anthony <147981830+wlmoi@users.noreply.github.com>
2024-07-20 21:32:26 +0700

    Initial commit

---
+++ src/app/contoh/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "~/components/ui/button";
+
+function Page() {
+  return (
+    <div>
+      <Button className="text-primary-foreground">Ini Button</Button>
+    </div>
+  );
+}
+
+export default Page;
